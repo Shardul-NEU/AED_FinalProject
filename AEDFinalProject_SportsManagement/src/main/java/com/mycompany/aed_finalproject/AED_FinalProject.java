edbf949e0863acd1e042ba972367b9c2b6efc579
Shardul <deshmukh.shard@northeastern.edu>
2022-12-01 17:34:06 -0500

    Database connection successful

--- AEDFinalProject_SportsManagement/src/main/java/com/mycompany/aed_finalproject/AED_FinalProject.java
+++ AEDFinalProject_SportsManagement/src/main/java/com/mycompany/aed_finalproject/AED_FinalProject.java
@@ -5,10 +5,12 @@
 
 package com.mycompany.aed_finalproject;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import java.util.Iterator;
 import org.bson.Document;
 
 /**
@@ -18,12 +20,20 @@ import org.bson.Document;
 public class AED_FinalProject {
 
     public static void main(String[] args) {
-        MongoClient client = (MongoClient) MongoClients.create("mongodb+srv://suraj:<password>@cluster0.ehrr7jd.mongodb.net/?retryWrites=true&w=majority");
+        MongoClient client = MongoClients.create("mongodb+srv://suraj:7021072380@cluster0.ehrr7jd.mongodb.net/?retryWrites=true&w=majority");
         MongoDatabase database = client.getDatabase("SportsManagement");
         
 //      If want to connect to new collection create instacne below this.
 //      And pass the object to other class.
         MongoCollection<Document> admin = database.getCollection("admin");
 //      collection initialization ends here
+// The following code is to test the connection
+        FindIterable<Document> iterDoc = admin.find();
+      Iterator it = iterDoc.iterator();
+      while (it.hasNext()) {
+         System.out.println(it.next());
+
+    }
     }
 }
+    
